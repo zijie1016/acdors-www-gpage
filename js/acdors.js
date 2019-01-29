3ca2679d834bc0eb12d2798cf71eac43f0133f95
yezijie <yezijie@solisopia.com>
2019-01-29 18:44:19 +0800

    Finish overlay UIs

--- js/acdors.js
+++ js/acdors.js
@@ -2,7 +2,6 @@
 
 // URL全局变量
 var ossUrl = 'https://acdors.oss-cn-beijing.aliyuncs.com/';
+var domainUrl = 'http://dev.api.acdors.com/';
 // var domainUrl = '/api/';
-var domainUrl = 'http://api.acdors.com/';
-// var domainUrl = 'http://dev.api.acdors.com/';
 
