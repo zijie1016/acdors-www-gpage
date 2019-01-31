0074b689e267ef9025746ee6e9a4654877bdee3b
yezijie <yezijie@solisopia.com>
2019-01-31 16:57:56 +0800

    完成分享页功能

--- js/acdors.js
+++ js/acdors.js
@@ -4,3 +4,10 @@
 var ossUrl = 'https://acdors.oss-cn-beijing.aliyuncs.com/';
 var domainUrl = '/api/';
 
+//获取连接字符串
+function GetQueryString(name)
+{
+  var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+  var r = window.location.search.substr(1).match(reg);
+  if(r!=null)return  decodeURI(r[2]); return null;
+}
