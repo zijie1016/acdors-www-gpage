a600f92a3bbeb78205348982230a2936dcca326f
yezijie <yezijie@solisopia.com>
2019-02-10 02:01:39 +0800

    Using npm gulp and browser-sync

--- js/acdors.js
+++ js/acdors.js
@@ -2,7 +2,8 @@
 
 // URL全局变量
 var ossUrl = 'https://acdors.oss-cn-beijing.aliyuncs.com/';
-var domainUrl = '/api/';
+// var domainUrl = '/api/';
+var domainUrl = 'http://dev.api.acdors.com/';
 
 //获取连接字符串
 function GetQueryString(name)
